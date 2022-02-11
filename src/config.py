fd8743ef903ecbc3106eb5d792a14b40b7da1f25
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-02-11 21:59:55 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import torch
 
-ALPHABET = "абвгдежзийклмнопрстуфхцчшщъыьэюяё-"
+ALPHABET = "%(),-./0123456789:;?[]«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё-"
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 REPORT_ACCURACY = False
 PATH_TO_IMGDIR = "/content/images/"
