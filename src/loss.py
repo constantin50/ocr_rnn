576e6da19ea5bf065fa44af4cf76581aec95cac0
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-02-13 14:08:18 +0700

    Update loss.py

--- src/loss.py
+++ src/loss.py
@@ -8,9 +8,7 @@ class CustomCTCLoss(torch.nn.Module):
         self.dim = dim
         self.ctc_loss = torch.nn.CTCLoss(reduction='mean', zero_infinity=True)
 
-    def forward(self, logits, labels,
-            prediction_sizes, target_sizes):
-        EPS = 1e-7
+    def forward(self, logits, labels, prediction_sizes, target_sizes):
         loss = self.ctc_loss(logits, labels, prediction_sizes, target_sizes)
         loss = self.sanitize(loss)
         return self.debug(loss, logits, labels, prediction_sizes, target_sizes)
@@ -18,13 +16,14 @@ class CustomCTCLoss(torch.nn.Module):
     def sanitize(self, loss):
         EPS = 1e-7
         if abs(loss.item() - float('inf')) < EPS:
+            print("inf loss: batch has been zero out")
             return torch.zeros_like(loss, requires_grad = True)
         if math.isnan(loss.item()):
+            print("nan loss: batch has been zero out")
             return torch.zeros_like(loss, requires_grad = True)
         return loss
 
-    def debug(self, loss, logits, labels,
-            prediction_sizes, target_sizes):
+    def debug(self, loss, logits, labels, prediction_sizes, target_sizes):
         if math.isnan(loss.item()):
             print("Loss:", loss)
             print("logits:", logits)
@@ -32,4 +31,4 @@ class CustomCTCLoss(torch.nn.Module):
             print("prediction_sizes:", prediction_sizes)
             print("target_sizes:", target_sizes)
             raise Exception("NaN loss obtained.")
-        return loss
+        return losss
