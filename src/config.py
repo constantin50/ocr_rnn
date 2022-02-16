8be4baed9990695b33f48d766fb8012dc3cde3cd
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-02-16 22:31:26 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 import torch
 
-ALPHABET = "%(),-./0123456789:;?[]«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё-"
+ALPHABET = " %(),-./0123456789:;?[]«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё-"
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 REPORT_ACCURACY = False
 PATH_TO_IMGDIR = "/content/images/"
 PATH_TO_LABELS = "/content/labels/"
+PATH_TO_CHECKPOINT = "/content/checkpoint.pt"
